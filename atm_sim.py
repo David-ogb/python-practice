e721b48d30a8c1dd5225d04d4fad25c0aefa8a30
David-ogb <davidogbodo71@gmail.com>
2026-07-09 16:36:49 +0000

    A basic ATM machine --version 1.0

--- atm_sim.py
+++ atm_sim.py
@@ -1,60 +1,39 @@
-balance = 10
-
-# to redo the programme
-def get_choice():
-    print(balance)
-    user_input = int(input("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\nEnter an option to begin: "))
-    # to exit the program
-    while user_input <= 4:
-        try:
-            if user_input == 4:
-                print('Thankyou for banking with us')
-                break
-            elif user_input == 1:
-                Deposit()
-            elif user_input == 2:
-                Withdraw()
-            elif user_input == 3:
-                print(f"Your balance is {balance}")
-                get_choice()
-        except ValueError:
-            print("Please enter within the range of 1 - 3")
-            return
-
+balance = 0
 # deposting function
-def Deposit():
-     balance = 0 
+def Deposit(val):
      deposit = int(input("Enter your deposit: "))
-     balance += deposit
-     print(f"Your balance is {balance:.2f}")
-     get_choice()
-
+     val += deposit
+     print(f"You have sucessfully deposited {deposit:.2f}")
+     return val
+     
+    
 # to withdraw
-def Withdraw():
-    balance = 0 
+def Withdraw(val):    
     withdraw = int(input("How much do you want to withdraw: "))
-    balance -= withdraw
-    print(f"Your balance is {balance}")
-    get_choice()
+    val -= withdraw
+    return val
+    # print(f"Your balance is {balance}")
 
 # function to display the menu
-def menu():
+def menu(balance):
+    
     user_input = int(input("1. Deposit\n2. Withdraw\n3. Check Balance\n4. Exit\nEnter an option to begin: "))
     # to exit the program
-    while True:
-        try:
-            if user_input == 4:
-                print('Thankyou for banking with us')
-                break
-            elif user_input == 1:
-                Deposit()
-            elif user_input == 2:
-                Withdraw()
-            elif user_input == 3:
-                print(f"Your balance is {balance}")
-                get_choice()
-        except ValueError:
-            print("Please enter within the range of 1 - 3")
-            return
+    if user_input == 4:
+        print('Thankyou for banking with us')
+        return
+    elif user_input == 1:
+            balance = Deposit(balance)
+            print(f"Your balance is {balance:.2f}")
+    elif user_input == 2:
+        balance = Withdraw(balance)
+        print(f"Your balance is {balance:.2f}")
+    elif user_input == 3:
+        print(f"Your balance is {balance}")
+    return balance
 
-menu()
+balance = menu(balance)
+menu(balance)
+balance = menu(balance)
+# check if it's saved to balance
+print(balance)
